4bb06b37eb7539c87e6b61240d4197ef74d28e2b
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2025-02-11 14:48:09 +0530

    code refactor

--- SmartAngularApp/SmartAngularApp/ClientApp/src/main.ts
+++ SmartAngularApp/SmartAngularApp/ClientApp/src/main.ts
@@ -8,14 +8,11 @@ import {
 import { FormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
 import { HomeComponent } from './app/home/home.component';
-import { OcrComponent } from './app/ocr/ocr.component';
 import { AppComponent } from './app/app.component';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
-
-const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
 if (environment.production) {
   enableProdMode();
@@ -27,7 +24,11 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'computer-vision-ocr', component: OcrComponent },
+      {
+        path: 'computer-vision-ocr',
+        loadComponent: () =>
+          import('./app/ocr/ocr.component').then((m) => m.OcrComponent),
+      },
     ]),
   ],
 }).catch((err) => console.log(err));
